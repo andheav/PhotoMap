3dad43b3456aaa09454aec4ac7cdf56679913d35
andheav <andrewheavirland@yahoo.com>
2018-12-10 11:39:31 -0600

    Added custom zoom levels and new centers for some images

--- js/funcs.js
+++ js/funcs.js
@@ -1,32 +1,32 @@
 const photos = [
-  ["DSC02063",45.245474,-92.986970,"landscape"],
-  ["DSC02072",45.245474,-92.986970,"landscape"],
-  ["DSC02110",45.246647,-92.991829,"portrait"],
-  ["DSC02186",45.258186,-92.918164,"landscape"],
-  ["DSC02222",45.271285,-92.877985,"landscape"],
-  ["DSC02241",45.272649,-92.866223,"portrait"],
-  ["DSC02276",45.284104,-92.765107,"portrait"],
-  ["DSC02290",45.288164,-92.761261,"portrait"],
-  ["DSC02297",45.312242,-92.725622,"landscape"],
-  ["DSC02345",45.322028,-92.718996,"portrait"],
-  ["DSC02378",45.345976,-92.703577,"portrait"],
-  ["DSC02392",45.370067,-92.693965,"landscape"],
-  ["DSC02399",45.406701,-92.652973,"portrait"],
-  ["DSC02506",45.473052,-92.913599,"landscape"],
-  ["DSC02576",45.47857,-93.051599, "landscape"],
-  ["DSC02599",45.484494,-93.050753,"landscape"],
-  ["DSC02623",45.484434,-93.050516,"portrait"],
-  ["DSC02637",45.484434,-93.050516,"landscape"],
-  ["DSC02681",45.484434,-93.050516,"landscape"],
-  ["DSC02752",45.484494,-93.050753,"landscape"],
-  ["DSC02862",45.484432,-93.0506,  "landscape"],
-  ["DSC02969",45.484906,-93.050891,"landscape"],
-  ["DSC03067",45.245545,-92.987073,"landscape"],
-  ["DSC03108",45.245545,-92.987073,"landscape"],
-  ["DSC03126",45.245545,-92.987073,"landscape"],
-  ["DSC03172",45.245545,-92.987073,"landscape"],
-  ["DSC03180",45.245545,-92.987073,"portrait"],
-  ["DSC03182",45.245545,-92.987073,"landscape"]
+  ["DSC02063",45.245474,-92.986970,"landscape", 17, null, null],
+  ["DSC02072",45.245474,-92.986970,"landscape", 17, null, null],
+  ["DSC02110",45.246647,-92.991829,"portrait", 16, null, null],
+  ["DSC02186",45.258186,-92.918164,"landscape", 15, 45.257075, -92.915957],
+  ["DSC02222",45.271285,-92.877985,"landscape", 16, null, null],
+  ["DSC02241",45.272649,-92.866223,"portrait", 16, 45.271351, -92.864745],
+  ["DSC02276",45.284104,-92.765107,"portrait", 15, 45.281133, -92.760860],
+  ["DSC02290",45.288164,-92.761261,"portrait", 15, 45.283101, -92.760104],
+  ["DSC02297",45.312242,-92.725622,"landscape", 15, null, null],
+  ["DSC02345",45.322028,-92.718996,"portrait", 15, 45.321829, -92.713144],
+  ["DSC02378",45.345976,-92.703577,"portrait", 15, 45.347749, -92.698631],
+  ["DSC02392",45.370067,-92.693965,"landscape", 13, null, null],
+  ["DSC02399",45.406701,-92.652973,"portrait", 15, 45.409986, -92.649652],
+  ["DSC02506",45.473052,-92.913599,"landscape", 14, 45.477103, -92.905793],
+  ["DSC02576",45.47857,-93.051599, "landscape", 15, null, null],
+  ["DSC02599",45.484494,-93.050753,"landscape", 18, null, null],
+  ["DSC02623",45.484434,-93.050516,"portrait", 19, null, null],
+  ["DSC02637",45.484434,-93.050516,"landscape", 19, null, null],
+  ["DSC02681",45.484434,-93.050516,"landscape", 19, null, null],
+  ["DSC02752",45.484494,-93.050753,"landscape", 19, null, null],
+  ["DSC02862",45.484631,-93.050602,  "landscape", 18, null, null],
+  ["DSC02969",45.484906,-93.050891,"landscape", 17, 45.484657, -93.050612],
+  ["DSC03067",45.245545,-92.987073,"landscape", 17, null, null],
+  ["DSC03108",45.245545,-92.987073,"landscape", 17, null, null],
+  ["DSC03126",45.245545,-92.987073,"landscape", 17, null, null],
+  ["DSC03172",45.245545,-92.987073,"landscape", 17, null, null],
+  ["DSC03180",45.245545,-92.987073,"portrait", 17, null, null],
+  ["DSC03182",45.245545,-92.987073,"landscape", 17, null, null]
 ];
 
 function getIndex(cur_photo) {
@@ -102,7 +102,7 @@ function previousPhoto() {
 var map;
 function initMap() {
   var airport = {lat: 45.245545, lng: -92.987073};
-  map = new google.maps.Map(document.getElementById('map'), {center: airport, zoom: 16, mapTypeId: 'satellite', rotateControl: true});
+  map = new google.maps.Map(document.getElementById('map'), {center: airport, zoom: 17, mapTypeId: 'satellite', mapTypeControl: false, streetViewControl: false});
   var marker = new google.maps.Marker({position: airport, map: map});
   plotMarkers()
 }
@@ -113,6 +113,20 @@ function plotMarkers() {
   }
 }
 
+function hasNewCenter(pos) {
+  if (photos[pos][5] == null && photos[pos][6] == null) {
+    return false
+  } else {
+    return true
+  }
+}
+
 function moveMap(pos) {
-  map.setCenter({lat: photos[pos][1], lng: photos[pos][2]});
+  if (hasNewCenter(pos)) {
+    map.setCenter({lat: photos[pos][5], lng: photos[pos][6]});
+  } else {
+    map.setCenter({lat: photos[pos][1], lng: photos[pos][2]});
+  }
+  console.log(photos[pos][0] + ": " + photos[pos][1] + ", " + photos[pos][2]);
+  map.setZoom(photos[pos][4]);    // This is the function to use to change zooms
 }
